66d4149484128e2a3c73ae84e71ccef0d5c2c3d8
jakemulley <jakemulley@users.noreply.github.com>
2021-10-02 02:35:21 +0100

    Pin provider versions

--- versions.tf
+++ versions.tf
@@ -1,11 +1,17 @@
 terraform {
   required_providers {
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
+      version = ">=2.2.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">=3.6.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">=2.1.0"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.15"
 }
