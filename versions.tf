f59fc2e7d178e2ce23723dcc24bba7e34476576a
jakemulley <jakemulley@users.noreply.github.com>
2023-12-27 09:52:14 +0000

    Standardise README

--- versions.tf
+++ versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = ">=2.2.0"
+      version = ">= 2.4.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">=3.6.0"
+      version = ">= 5.0.0"
     }
     external = {
       source  = "hashicorp/external"
-      version = ">=2.1.0"
+      version = ">= 2.3.0"
     }
   }
-  required_version = ">= 0.15"
+  required_version = ">= 1.0"
 }
